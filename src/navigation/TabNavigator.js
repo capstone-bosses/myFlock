fe5e13375761d596373a8048b01203a274c6095f
dalgonaio <8888888dalgonaio@users.noreply.github.com>
2021-10-13 18:00:38 -0400

    Feat: chat

--- src/navigation/TabNavigator.js
+++ src/navigation/TabNavigator.js
@@ -6,6 +6,7 @@ import AddInterest from '../screens/AddInterest';
 import LoginScreen from '../screens/Login';
 import EditProfileScreen from '../screens/EditProfileScreen';
 import ProfileView from '../screens/ProfileView';
+import NestViewScreen from '../screens/NestViewScreen'
 
 //whatever you do, MAKE SURE YOU ADD A PERIOD IN BETWEEN TAB.NAVIGATOR ELSE IT WILL NOT COMPILE
 //YOU WILL SPEND HOURS WITH A BLACK SCREEN AND NO CONSOLE.LOG NOT KNOWING WHAT HAPPENED
@@ -17,7 +18,7 @@ function TabNavigator(props) {
 			<Tab.Screen name="StackNavigator" component={StackNavigator} />
 			<Tab.Screen name="Add Interest" component={AddInterest} />
 			<Tab.Screen name="Login" component={LoginScreen} />
-			<Tab.Screen name="Edit Profile" component={EditProfileScreen} />
+			<Tab.Screen name="Nest View" component={NestViewScreen} />
 			<Tab.Screen name="Profile View" component={ProfileView} />
 		</Tab.Navigator>
 	);
