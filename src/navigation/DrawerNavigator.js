8d3100b84cfca42d07a61fecf4ea547d327c9da5
Audrey Maldonado <audrey.maldonado@gmail.com>
2021-10-13 15:34:51 -0400

    work toward geolocation services

--- src/navigation/DrawerNavigator.js
+++ src/navigation/DrawerNavigator.js
@@ -3,6 +3,7 @@ import { createDrawerNavigator } from '@react-navigation/drawer';
 import TabNavigator from './TabNavigator';
 import { View, StyleSheet, Text } from 'react-native';
 import InitialLocation from '../maps/InitialLocation';
+import UserLocation from '../maps/UserLocation'
 
 const Drawer = createDrawerNavigator();
 
@@ -15,6 +16,7 @@ function DrawerNavigator() {
 		>
 			<Drawer.Screen name="Tabs" component={TabNavigator} />
 			<Drawer.Screen name="InitialLocation" component={InitialLocation} />
+			<Drawer.Screen name="User Location" component={UserLocation} />
 		</Drawer.Navigator>
 	);
 }
