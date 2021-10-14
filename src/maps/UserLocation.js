fa0268a842657d2689e5e80277bc9d803a84af94
Audrey Maldonado <audrey.maldonado@gmail.com>
2021-10-14 11:42:12 -0400

    Update UserLocation.js

--- src/maps/UserLocation.js
+++ src/maps/UserLocation.js
@@ -1,28 +1,66 @@
 import React, { useState, useEffect } from 'react';
-import {View, Text, Button} from 'react-native'
-import MapView, { Marker } from 'react-native-maps';
-import Geolocation from 'react-native-geolocation-service';
+import { Platform, Text, View, StyleSheet } from 'react-native';
+import * as Location from 'expo-location';
 
+export default function UserLocation() {
+  const [location, setLocation] = useState(null);
+  const [errorMsg, setErrorMsg] = useState(null);
 
-const UserLocation = () => {
+  useEffect(() => {
+    (async () => {
+      let { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== 'granted') {
+        setErrorMsg('Permission to access location was denied');
+        return;
+      }
+
+      let location = await Location.getCurrentPositionAsync({});
+      setLocation(location);
+    })();
+  }, []);
+
+  let text = 'Waiting..';
+  if (errorMsg) {
+    text = errorMsg;
+  } else if (location) {
+    text = JSON.stringify(location);
+
+    console.log(text)
+  }
 
   return (
-
-    <>
-    <View style={{marginTop: 10, padding: 10, borderRadius: 10, width: '40%'}}>
-      <Button title="Get Location"/>
-        </View>
-        <Text>Latitude: </Text>
-        <Text>Longitude: </Text>
-        <View style={{marginTop: 10, padding: 10, borderRadius: 10, width: '40%'}}>
-        <Button title="Send Location"/>
+    <View style={styles.container}>
+      <Text style={styles.paragraph}>{text}</Text>
+      {/* <Text style={styles.location}>You're located at {location.coords.latitude}, {location.coords.longitude}</Text> */}
     </View>
-    </>
-);
+  );
 }
 
+const styles = StyleSheet.create({
+	container: {
+		flex: 1,
+		alignItems: 'center',
+		justifyContent: 'center',
+		padding: 10,
+	},
+  location: {
+    color: 'blue',
+  },
+	paragraph: {
+    color: 'red',
+  }
+});
 
+//works on android and ios emulator
 
-export default UserLocation;
-
-
+//location looks like:
+// {"coords": {
+//   "altitude":0,
+//   "altitudeAccuracy":-1,
+//   "latitude":37.785834,
+//   "accuracy":5,
+//   "longitude":-122.406417,
+//   "heading":-1,
+//   "speed":-1},
+//   "timestamp":1634175482491.719
+// }
